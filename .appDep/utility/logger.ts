2ec664c726e4246024c7d322b88342786a0e91bd
jomoartz <johnjohananmogbolu@gmail.com>
2025-02-10 21:22:30 +0100

    restarting project

---
+++ .appDep/utility/logger.ts
@@ -0,0 +1,16 @@
+// import Bugsnag, { NotifiableError } from '@bugsnag/expo';
+
+// const log = (error: NotifiableError) => {
+//   if (process.env.NODE_ENV === 'development') {
+//     console.log(error);
+//   }
+//   else {
+//   Bugsnag.notify(error);}}
+
+// const start = () => {
+//   Bugsnag.start({
+//     apiKey: process.env.EXPO_PUBLIC_BUGSNAG_API_KEY,
+//   });
+// };
+
+// export { log, start };
