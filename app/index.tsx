e26ac6aa124a48af82f2d74af665d5868c27eb4e
jomoartz <johnjohananmogbolu@gmail.com>
2025-02-10 23:56:50 +0100

    feature/set up index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ReactNode } from "react";
+import { Screen } from "@/components/Screen";
 
-export default function Index() {
+type Screen = {
+  children: ReactNode;
+};
+
+const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Screen>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.text}>connect - Learn - inform</Text>
+        <Text style={styles.text2}>Do it yourself</Text>
+      </View>
+    </Screen>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: 100,
+    width: 300,
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "open-sans",
+    marginTop: 30,
+  },
+  text2: {
+    fontSize: 20,
+    fontFamily: "open-sans",
+    fontStyle: "italic",
+    fontWeight: "bold",
+  },
+});
+
+export default Index;
